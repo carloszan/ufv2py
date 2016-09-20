c39a71b516cb4eba4cb0757eb03c8f6216d50177
Carlos <undefined751@gmail.com>
2016-09-19 23:30:41 -0300

    initial gui

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
 # -*- coding: utf-8 -*-
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget
 
 
-class Window():
-    pass
+class Window(QMainWindow):
+
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.__initUI__()
+
+    def __initUI__(self):
+        self.setGeometry(350, 200, 600, 500)
+        self.setWindowTitle("ufv2py")
+        self.central_widget = QStackedWidget()
+        self.setCentralWidget(self.central_widget)
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    # Start the app
+    GUI = Window()
+    GUI.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
